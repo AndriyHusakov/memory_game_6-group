341e07efc4f86fe85341944ea8ee00a055e35ac8
AndriyHusakov <116489043+AndriyHusakov@users.noreply.github.com>
2023-04-14 18:05:50 +0300

    Зробив таймер який тільки запускається

--- script.js
+++ script.js
@@ -9,44 +9,44 @@ let persons = [
     img: "./img/yosif.png"
   },
   {
-    name: "Левко Лук'яненко",
-    img: "http://surl.li/gdznp"
+    name: "Тарас Шевченко",
+    img: "./img/taras_shevchenko.png"
+  },
+  {
+    name: "Павло Тетеря",
+    img: "./img/Pavlo_Teteri.jpg"
+  },
+  {
+    name: "Іван Франко",
+    img: "./img/ivan_franko.png"
+  },
+  {
+    name: "Олесь Гончар",
+    img: "./img/oles.png"
+  },
+  {
+    name: "Василь Стус",
+    img: "./img/vasil.png"
+  },
+  {
+    name: "В’ячеслав Чорновіл",
+    img: "./img/vyacheslav.png"
+  },
+  {
+    name: "Іван Дзюба",
+    img: "./img/ivan.png"
+  },
+  {
+    name: "Іван Сірко",
+    img: "./img/Ivan_Sirko.png"
+  },
+  {
+    name: "Петро Григоренко",
+    img: "./img/petr.png"
   },
   {
     name: "Іван Миколайчук",
-    img: "http://surl.li/gdzoi"
-  },
-  {
-    name: "Павло Скоропатський",
-    img: "http://surl.li/gdzop"
-  },
-  {
-    name: "Симон Петлюра",
-    img: "http://surl.li/gdzot"
-  },
-  {
-    name: "Степан Бандера",
-    img: "http://surl.li/gbjzp"
-  },
-  {
-    name: "Євген Коновалець",
-    img: "http://surl.li/gblax"
-  },
-  {
-    name: "Левко Лук'яненко",
-    img: "http://surl.li/gdznp"
-  },
-  {
-    name: "Іван Миколайчук",
-    img: "http://surl.li/gdzoi"
-  },
-  {
-    name: "Павло Скоропатський",
-    img: "http://surl.li/gdzop"
-  },
-  {
-    name: "Симон Петлюра",
-    img: "http://surl.li/gdzot"
+    img: "./img/ivanm.png"
   }
 ];
 let tiles = [];
@@ -67,100 +67,60 @@ function create() {
     const tile = document.createElement("div");
     tile.classList.add("tile");
     tile.classList.add("border");
+		tile.setAttribute("id", i)
     tile.dataset.value = i;
-    // крайні справа або зліва можуть бути видалені
-    if ((i + 1) % 6 === 0) {
-      tile.dataset.removed = "true";
-      tile.dataset.side = "right";
-    } else if (i % 6 === 0) {
-      tile.dataset.removed = "true";
-      tile.dataset.side = "left";
-    } else {
-      tile.dataset.removed = "false";
-      tile.dataset.side = "center";
-    }
-    tile.style.backgroundImage = `url(${persons[i].img})`;
+    tile.style.backgroundImage = `url(./img/guess.png)`;
     tile.addEventListener("click", choose);
     tiles.push(tile);
   }
   tiles.forEach((tile) => board.appendChild(tile));
 }
+
 let chosen = [];
-let rowRemoveCount = [0, 0, 0, 0];
+
 create();
 
 function choose() {
-  if (chosen.length < 2 && this.dataset.value !== chosen[0]) {
-    chosen.push(this.dataset.value);
-    this.classList.remove("border");
-    this.classList.add("choosen");
-  }
-  if (chosen.length === 2) {
-    setTimeout(check, 1000);
-  }
+	if (chosen.length < 2 && this.dataset.value !== chosen[0]){
+		this.classList.remove("border")
+		this.classList.add("choosen")
+		chosen.push(this.dataset.value)
+		this.style.backgroundImage = `url(${persons[this.dataset.value].img})`
+		console.log(chosen)
+	}
+	if (chosen.length === 2){
+		setTimeout(check, 1000)
+	}
 }
 
-function check() {
-  // якщо імена співпадають та це крайні картки
-  console.log(tiles[chosen[0]]);
-  console.log(tiles[chosen[1]]);
-  if (
-    persons[chosen[0]].name === persons[chosen[1]].name &&
-    tiles[chosen[0]].dataset.removed === "true" &&
-    tiles[chosen[1]].dataset.removed === "true"
-  ) {
-    tiles[chosen[0]].style.visibility = "hidden";
-    tiles[chosen[1]].style.visibility = "hidden";
+function check(){
+	if(persons[chosen[0]].name === persons[chosen[1]].name){
+		tiles[chosen[0]].style.visibility = "hidden"
+		tiles[chosen[1]].style.visibility = "hidden"
+    chosen = []
+	 }
+	 else{
+		tiles[chosen[0]].classList.remove("choosen")
+		tiles[chosen[0]].classList.add("border")
+		tiles[chosen[1]].classList.remove("choosen")
+		tiles[chosen[1]].classList.add("border")
+		tiles[chosen[0]].style.backgroundImage = `url(./img/guess.png)`;
+		tiles[chosen[1]].style.backgroundImage = `url(./img/guess.png)`;
+	  chosen = []
+	 }
+}
 
-    // передати можливість для сусідньої картки
-    // контроль рядів ----- rowRemoveCount
-
-    for (let j = 0; j < 2; j++) {
-      let rowStart = 0
-      let rowEnd = 5
-      for (let i = 0; i < rowRemoveCount.length; i++) {
-        if (+chosen[j] >= rowStart && +chosen[j] <= rowEnd) {
-          if (rowRemoveCount[i] < 5) {
-            if (tiles[chosen[j]].dataset.side === "left") {
-              tiles[+chosen[j] + 1].dataset.side = "left";
-              tiles[+chosen[j] + 1].dataset.removed = "true";
-              rowRemoveCount[i] += 1;
+function init()
+            {
+                sec = 0;
+                setInterval(tick, 1000);
             }
-            if (tiles[chosen[j]].dataset.side === "right") {
-              tiles[+chosen[j] - 1].dataset.side = "right";
-              tiles[+chosen[j] - 1].dataset.removed = "true";
-              rowRemoveCount[i] += 1;
+            
+            function tick()
+            {
+                sec++;
+                document.getElementById("timer").
+                    childNodes[0].nodeValue = sec;
             }
-          }
-        }
-        rowStart += 6;
-        rowEnd += 6;
-      }
-    }
-    chosen = [];
-    console.log(rowRemoveCount);
-  } else {
-    tiles[chosen[0]].classList.remove("choosen");
-    tiles[chosen[0]].classList.add("border");
-    tiles[chosen[1]].classList.remove("choosen");
-    tiles[chosen[1]].classList.add("border");
-    chosen = [];
-  }
-}
-let fon = new Audio();
-fon.src = "kossacks.mp3";
-fon.play();
-setInterval(fon.play(), 1500000);
 
 
-let reset = document.querySelector(".reset")
-reset.addEventListener('click',
-function(){
-  for(let i = 0; i < tiles.length; i++){
- tiles[i].style.visibility = "visible"
- tiles[i].classList.remove("choosen");
- tiles[i].classList.add("border");
-}
-}
-)
-// Гусаков Андрій
